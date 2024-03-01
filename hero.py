b73f7033d2ffc2a8bd66031e8e70510abdbb547f
Wizqqr <132731212+Wizqqr@users.noreply.github.com>
2024-03-01 17:29:18 +0600

    heroц.py

--- hero.py
+++ hero.py
@@ -11,9 +11,12 @@ class SuperHero:
         return(self.name)
     def double_hp(self):
         return(self.health_points * 2)
-    def full_hero(self):
-        return f"Имя героя: {self.name}, Прозвище: {self.nickname}, Суперспособность: {self.superpower}, Здоровье: {self.health_points} Фраза: {self.catchphrase}"
-
+    def __str__(self):
+        return(f'name: {self.name}\n'
+               f'nickname: {self.nickname}\n'
+               f'superpower: {self.superpower}\n'
+               f'health_points: {self.health_points}\n'
+               f'catchphrase: {self.catchphrase}')
 hero = SuperHero("Cristiano Ronaldo", "CR7", "Goal Machine", 100, "I'm the best")
 print(hero.sound_name())
 print(hero.double_hp())
